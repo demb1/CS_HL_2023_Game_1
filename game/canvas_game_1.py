8817b93917cb7ab992bc4c0d0cf0b3cf8d4da008
AD <adam.w.dembinski@icloud.com>
2023-04-25 19:45:09 +0200

    Added images

--- game/canvas_game_1.py
+++ game/canvas_game_1.py
@@ -10,24 +10,41 @@ canvas.pack()
 app.bind()
 app.title('Run away game version 1.1.1')
 
+
+#guns
 frameCnt = 3
 frames = [PhotoImage(file='gun_gif_1.gif', format='gif -index %i' % i) for i in range(frameCnt)]
 
-def update(ind):
+def update1(ind):
 
     frame = frames[ind]
     ind += 1
     if ind == frameCnt:
         ind = 0
     gun_label.configure(image=frame)
-    app.after(10000, update, ind)
+    gun2_label.configure(image=frame)
+    gun3_label.configure(image=frame)
+    gun4_label.configure(image=frame)
+    app.after(10000, update1, ind)
 gun_label = Label(app)
 gun_label.pack()
 gun_label.place(x=0, y=100)
-app.after(0, update, 0)
+app.after(0, update1, 0)
+gun2_label = Label(app)
+gun2_label.pack()
+gun2_label.place(x=0, y=200)
+app.after(0, update1, 0)
+gun3_label = Label(app)
+gun3_label.pack()
+gun3_label.place(x=0, y=300)
+app.after(0, update1, 0)
+gun4_label = Label(app)
+gun4_label.pack()
+gun4_label.place(x=0, y=400)
+app.after(0, update1, 0)
 
 
-
+#timer
 def update_timer(seconds):
     minutes = seconds // 60
     seconds = seconds % 60
@@ -45,10 +62,16 @@ timer_label.place(x=750, y=10)
 
 start_timer(0)
 
-image = Image.open("mario.png")
-resize_image = image.resize((50, 50))
+#display heart
+heart = Image.open("heart.png")
+resize_heart = heart.resize((40, 40))
+heart_img = ImageTk.PhotoImage(resize_heart)
+heartDisplay = canvas.create_image(25, 25, image=heart_img)
 
-mario_img = ImageTk.PhotoImage(resize_image)
+image = Image.open("mario.png")
+resize_hero = image.resize((50, 50))
+
+mario_img = ImageTk.PhotoImage(resize_hero)
 hero = canvas.create_image(400, 250, image=mario_img)
 chaser1 = canvas.create_oval(250, 100, 200, 50, tags="chasser", fill="red", outline="yellow")
 chaser2 = canvas.create_oval(500, 100, 550, 150, tags="chasser", fill="red", outline="yellow")
@@ -57,7 +80,6 @@ chasers = [chaser1, chaser2]
 
 xspeed1 = -3
 yspeed1 = 3
-
 
 def move_chaser_1():
     global xspeed1, yspeed1
@@ -79,7 +101,6 @@ canvas.after(20, move_chaser_1)
 
 xspeed2 = 4
 yspeed2 = 4
-
 
 def move_chaser_2():
     global xspeed2, yspeed2
